5e2ebee98b1f36e91ff80f90e33c4fc46f74a6a4
Stanislav Gorchakov <stanislav.gorchakov@192.168.1.103>
2023-05-19 12:50:43 +0300

    add visualization

--- news_sources/types.py
+++ news_sources/types.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from dataclasses import dataclass
 
 
@@ -11,3 +12,14 @@ class News:
 @dataclass
 class NYTimesNews(News):
     pass
+
+
+class NewsColorsAndFonts(Enum):
+    LINE_COLOR = '#1fb6b6'
+    MAIN_COLOR = '#fff'
+    GRADIENT_COLOR = '#182419'
+
+    FONT_LINE = 'fonts/Bebas_Neue_Cyrillic.ttf'
+    FONT_MAIN = 'fonts/Bebas_Neue_Cyrillic.ttf'
+
+    INFO = 'ФУТБОЛ: НОВОСТИ И ОБЗОРЫ'
